7c1ead91e632a0a4c577815ff5dd25829fe5fdc4
Anamika Sharma <anamika.0601@gmail.com>
2020-10-12 13:37:26 -0700

    SJF Algorithm

---
+++ assignment2/algorithms/priority_queue.h
@@ -0,0 +1,14 @@
+#ifndef ASSIGNMENT2_PRIORITY_QUEUE
+#define ASSIGNMENT2_PRIORITY_QUEUE
+
+#include "job.h"
+
+// void print_heap(int A[]);
+
+void insert(process* key);
+
+// process * extract_min(process * A[]);
+process * peek();
+process* removeData();
+
+#endif //ASSIGNMENT2_PRIORITY_QUEUE
