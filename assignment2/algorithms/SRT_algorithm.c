c5b40e22bec47095e3c690876e96f98fd5f2ac0a
David Taylor <dct828@gmail.com>
2020-10-13 09:22:16 -0700

    Swaps SRT to use a job's remaining_time property instead of service_time

--- assignment2/algorithms/SRT_algorithm.c
+++ assignment2/algorithms/SRT_algorithm.c
@@ -12,16 +12,16 @@ static int SRTstart = 0,
 int SRTcomp(const void * v1, const void * v2) {
 	process *p1 = ((process *) v1);
 	process *p2 = ((process *) v2);
-	if((double) p1->service_time <= (double) p2->service_time) {
+	if((double) p1->remaining_time <= (double) p2->remaining_time) {
 		return -1;
 	}
-	else if((double) p1->service_time > (double) p2->service_time) {
+	else if((double) p1->remaining_time > (double) p2->remaining_time) {
 		return 1;
 	}
 	return 0;
 }
 
-static void SRTpush(process *job, int quanta) {
+static void SRTpush(process *job) {
 	SRTqueue[SRTend] = job;
 	SRTend = (SRTend + 1) % QUEUELEN;
 	if(SRTend > 1) {
@@ -29,7 +29,7 @@ static void SRTpush(process *job, int quanta) {
 	}
 }
 
-static process *SRTpop(int quanta) {
+static process *SRTpop() {
 	if (SRTend == 0) return NULL;
 	
 	process *ret = SRTqueue[0];
@@ -44,29 +44,30 @@ static process *SRTpop(int quanta) {
 static process *SRTcurrentJob = NULL;
 
 void SRT_Algorithm_Add(process *job, int quanta) {
+	job->remaining_time = job->service_time;
 	if (SRTcurrentJob)
-		if(SRTcurrentJob->end_time <= (job->service_time - 1 + quanta)) {
-			SRTpush(job, quanta);
+		if(SRTcurrentJob->end_time <= (job->remaining_time - 1 + quanta)) {
+			SRTpush(job);
 		}
 		else {
 			process *tempJob = SRTcurrentJob;
 			SRTcurrentJob = job;
-			SRTcurrentJob->end_time = quanta + fmax(SRTcurrentJob->service_time - 1, 0);
-			tempJob->service_time   = tempJob->end_time - quanta;
+			SRTcurrentJob->end_time = quanta + fmax(SRTcurrentJob->remaining_time - 1, 0);
+			tempJob->remaining_time   = tempJob->end_time - quanta;
 			tempJob->end_time       = 0;
-			SRTpush(tempJob, quanta);
+			SRTpush(tempJob);
 		}
 	else {
 		SRTcurrentJob = job;
-		SRTcurrentJob->end_time = quanta + fmax(SRTcurrentJob->service_time - 1, 0);
+		SRTcurrentJob->end_time = quanta + fmax(SRTcurrentJob->remaining_time - 1, 0);
 	}
 }
 
 int SRT_Algorithm(int quanta) {
 	if (SRTcurrentJob && SRTcurrentJob->end_time < quanta) SRTcurrentJob = NULL;
 	if (!SRTcurrentJob) {
-		SRTcurrentJob = SRTpop(quanta);
-		if (SRTcurrentJob) SRTcurrentJob->end_time = quanta + fmax(SRTcurrentJob->service_time - 1, 0);
+		SRTcurrentJob = SRTpop();
+		if (SRTcurrentJob) SRTcurrentJob->end_time = quanta + fmax(SRTcurrentJob->remaining_time - 1, 0);
 	}
 	if (!SRTcurrentJob) return -1;
 	
