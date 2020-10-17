a19957316c764ddaa59f57ccea45f0762ba84a1c
awhelan-school <awhelan@ucdavis.edu>
2020-10-17 13:39:53 -0700

    Update run.sh

--- assignment2/run.sh
+++ assignment2/run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 seed=1
+cd ..
 mkdir p2
 cd p2
 cmake ..
 make
-./p2/assignment2 $seed >> output.txt
-./p2/assignment2_rr $seed >> output.txt
-./p2/assignment2_hpf $seed >> output.txt
+../p2/assignment2 $seed >> output.txt
+../p2/assignment2_rr $seed >> output.txt
+../p2/assignment2_hpf $seed >> output.txt
