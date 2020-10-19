722c2da3d4593f797dd1d66f0131fb65696bc0c4
David Taylor <dct828@gmail.com>
2020-10-18 20:24:09 -0700

    Fixing concert issues

--- assignment3/concert.cpp
+++ assignment3/concert.cpp
@@ -5,27 +5,27 @@ Concert::Concert() {
     Concert::seatMutex = PTHREAD_MUTEX_INITIALIZER;
 }
 
-Concert::bool allocateSeat(Customer &customer, int row, int column) {
+bool Concert::allocateSeat(Customer &customer, int row, int column) {
     if(pthread_mutex_trylock(&seatMutex) == 0) {
         return false;
     }
     pthread_mutex_lock(&seatMutex);
-    seats[row][column] = customer;
+    seats[row][column] = &customer;
     pthread_mutex_unlock(&seatMutex);
     return true;
 }
 
-Concert::bool isRowFull(int row) {
-    for(int column = 0; i < 10; i++) {
-        if(seats[row][i] == nullptr) {
+bool Concert::isRowFull(int row) {
+    for(int column = 0; column < 10; column++) {
+        if(seats[row][column] == nullptr) {
             return false;
         }
     }
     return true;
 }
 
-Concert::bool isFull() {
-    for(int row = 0; row < 10; i++) {
+bool Concert::isFull() {
+    for(int row = 0; row < 10; row++) {
         if(!isRowFull(row)) {
             return false;
         }
