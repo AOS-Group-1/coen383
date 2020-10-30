eef2e2533ff03884e855e760f0d929dbb45763d5
David Taylor <dct828@gmail.com>
2020-10-30 15:57:08 -0700

    Code for LRU

--- assignment4/algorithms/LRU.cpp
+++ assignment4/algorithms/LRU.cpp
@@ -1,5 +1,14 @@
 #include "LRU.h"
 
 Page *LRU::getPage() {
-	return nullptr;
+	float lruTime = __INT_MAX__;
+	Page *evict = nullptr;
+
+	for(auto p: Page::pages) {
+		if(p->lastUsed < lruTime) {
+			lruTime = p->lastUsed;
+			evict = p;
+		}
+	}
+	return evict;
 }
