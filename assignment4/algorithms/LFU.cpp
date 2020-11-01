77572eeb11551b4380c9fcf781fb34ea50a56b68
meghascu <msharma3@scu.edu>
2020-10-31 20:03:20 -0700

    LFU Code

--- assignment4/algorithms/LFU.cpp
+++ assignment4/algorithms/LFU.cpp
@@ -1,5 +1,14 @@
 #include "LFU.h"
 
 Page *LFU::getPage() {
-	return nullptr;
+	int  min    = 0;
+	Page *evict = nullptr;
+	
+	for (auto p : Page::pages) {
+		if (p->n_ref < min) {
+			min   = p->n_ref;
+			evict = p;
+		}
+	}
+	return evict;
 }
