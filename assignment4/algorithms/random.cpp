516e1bb75f6da93073a315d03eb12f11ccb6055d
kshyani <61363088+kshyani@users.noreply.github.com>
2020-10-31 20:24:07 -0700

    Update random.cpp
    first commit

--- assignment4/algorithms/random.cpp
+++ assignment4/algorithms/random.cpp
@@ -1,5 +1,16 @@
 #include "random.h"
+#include <cstdlib>
 
 Page *Random::getPage() {
-	return nullptr;
+	int count = 0, max = -1;
+	int r = rand() % 100 + 1;
+	Page *evict = nullptr;
+    for(auto p: Page::pages){
+        if(p->n_ref > 0 && count < r){
+            max = p->n_ref;
+            evict = p;
+        }
+        count++;
+    }
+	return evict;
 }
